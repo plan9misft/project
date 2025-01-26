43cf6cfd733e8d2ab4b375b0039bbde627a9c5bf
plan9misft <plan9misft@gmail.com>
2025-01-25 17:03:09 -0800

    Update t.js

--- core/debuggers/.treio7/t.js
+++ core/debuggers/.treio7/t.js
@@ -1,2 +1,15 @@
-{[start=1]}
+{[start=1]} // this file is an operater engine 
 [;var u=U(t{RGB})]
+          import { onopen } from "module";
+          import { t } from "module";
+
+onopen *screenTop*1 // t
+{function name(params) {;var U=i({Int16Array})
+ t=~true
+f=~false         
+}}
+/* 
+.t
+*/ 
+[({RGBspace1 /*section "onopen" below*/"onopen"})]
+-t -y -l onend1
