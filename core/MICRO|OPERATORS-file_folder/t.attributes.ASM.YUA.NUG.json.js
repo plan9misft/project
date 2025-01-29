fc88afe6e9357132d75990dd37142762c4f92068
plan9misft <plan9misft@gmail.com>
2025-01-28 18:52:55 -0800

    Update and rename t.attributes.ASM.YUA.NUG.json.js to t.operator.d.attributes.ASM.YUA.NUG.json.js

--- core/MICRO|OPERATORS-file_folder/t.attributes.ASM.YUA.NUG.json.js
+++
@@ -1,2 +0,0 @@
-SERVER cvar `set` to 0();
-SERVER cvar `set-lag` to "lag_is_false"();
---
+++ core/MICRO|OPERATORS-file_folder/t.operator.d.attributes.ASM.YUA.NUG.json.js
@@ -0,0 +1,7 @@
+SERVER cvar `set` to 0();
+SERVER cvar `set-lag` to "lag_is_false"();
+SERVER [(A)tset=false]        name => "YUA"  r.tset=Tfalse.tpas||scr => '8274885739274994787e' ();
+OPERATOR [(B)tset=true]       name => "NVM_tester_#1"   t.tset=a.t3opfg||new i.this(0,0,0).LEAF_NVM();
+ASM-SERVER /* this operator uses macOS and windows, if your operating system is not windows or macOS you can still use this file and this github page.*/
+  [stalltoabove=true] ||t.f.h||windows_macOS => onMac="MACBOOK.json" |onwindows => File-Folder="system32" |infilefolder => @@ at "https://github.com/plan9misft/project/" @@ folder='project/core/debuggers' file="debugger1.d" [(usedebugger)tstart=true];
+LEAF-SERVER AserverWINDOWS `set` in command();
